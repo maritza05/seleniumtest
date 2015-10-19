296b2325654a6b454159110e28159be095548e39
Maritza <maritzaesparza05@gmail.com>
2015-10-18 22:45:59 -0500

    Añadido módulo time

--- pruebaCorreo.py
+++ pruebaCorreo.py
@@ -1,17 +1,20 @@
+# -*- coding: utf-8 -*-
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
+import time
+
 
 class TestCorreo(unittest.TestCase):
 
     def setUp(self):
         self.driver = webdriver.Firefox()
-        self.driver.get('https://login.live.com/login.srf?wa=wsignin1.0&rpsnv=12&ct=1445012473&rver=6.4.6456.0&wp=MBI_SSL_SHARED&wreply=https:%2F%2Fmail.live.com%2Fdefault.aspx&lc=2058&id=64855&mkt=es-mx&cbcxt=mai')
+        self.driver.get('https://mail.live.com?mkt=us-us')
 
     def test_prueba_titulo_de_outlook(self):
-        self.assertEqual('Iniciar session', self.driver.title)
+        self.assertEqual('Sign In', self.driver.title)
 
-    def test_prueba_busqueda_de_un_termino_ingenieria_software(self):
+    def test_prueba_busqueda_de_ingresar_correo(self):
         username = self.driver.find_element_by_id('i0116')
         username.send_keys('mariesparza426@outlook.com')
 
@@ -21,21 +24,24 @@ class TestCorreo(unittest.TestCase):
         btn_ok = self.driver.find_element_by_id('idSIButton9')
         btn_ok.click()
 
-        # click al primer checkbox encontrado
-        mensajes =  self.driver.find_elements_by_class_name('mlCheckbox')
-        mensajes[0].click()
+        time.sleep(10)
+
+        # click al primer checkbox encontrado en la bandeja de correo
+        print  self.driver.title
+        check_messages =  self.driver.find_elements_by_class_name('mlCheckbox')
+        check_messages[0].click()
+
+        time.sleep(10)
 
         # abrir un mensaje
-        mensajes2 =  self.driver.find_element_by_id('ukzWdDdJVz5RGTdmw75afbaQ2')
-        mensajes2.click()
-        self.assertEqual(1,1)
+        message =  self.driver.find_element_by_id('ukzWdDdJVz5RGTdmw75afbaQ2')
+        message.click()
 
-        #opciones
-        opciones = self.driver.find_element_by_id('inboxControl0fv-ReadMessageContainer')
+        # menu de opciones disponibles al abrir un correo
+        options_from_email = self.driver.find_element_by_id('inboxControl0fv-ReadMessageContainer')
+        options_from_email_exist = options_from_email is None
 
-        print opciones == None
-
-
+        self.assertEqual(options_from_email_exist, False)
 
 
 if __name__=='__main__':
